24be1db0fd538b5c610525341a89374959c93923
Boris Petrov <boris_petrov@live.com>
2020-12-18 11:11:07 +0200

    Add a (failing) test for observers

--- tests/integration/use-test.js
+++ tests/integration/use-test.js
@@ -1,5 +1,7 @@
 import { module, test } from 'qunit';
 import { tracked } from 'tracked-built-ins';
+import EmberObject from '@ember/object';
+import { observes } from '@ember-decorators/object';
 
 import { use, Resource } from 'ember-could-get-used-to-this';
 
@@ -44,5 +46,38 @@ module('@use', () => {
     instance.text = 'world';
 
     assert.equal(instance.test, 'world');
+  });
+
+  test('can observe values', async function (assert) {
+    let didChange = false;
+
+    class TestResource extends Resource {
+      @tracked value;
+
+      setup() {
+        this.value = this.args.positional[0];
+      }
+    }
+
+    class MyClass extends EmberObject {
+      @tracked text = 'hello'
+
+      @use test = new TestResource(() => [this.text])
+
+      @observes('test')
+      observer() {
+        didChange = true;
+      }
+    }
+
+    let instance = new MyClass();
+
+    assert.equal(instance.test, 'hello');
+
+    instance.text = 'world';
+
+    assert.equal(instance.test, 'world');
+
+    assert.equal(didChange, true);
   });
 });
