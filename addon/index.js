ed320c957c9c319eb4514cb055b6b929fd160054
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-08 10:59:52 -0500

    Revert invisible character changes

--- addon/index.js
+++ addon/index.js
@@ -1,25 +1,25 @@
-import {invokeHelper} from '@ember/helper';
-import {getValue} from '@glimmer/tracking/primitives/cache';
+import { invokeHelper } from '@ember/helper';
+import { getValue } from '@glimmer/tracking/primitives/cache';
 
-export {modifier, Modifier} from './-private/modifiers';
-export {Resource} from './-private/resources';
+export { modifier, Modifier } from './-private/modifiers';
+export { Resource } from './-private/resources';
 
 export function use(prototype, key, desc) {
   let resources = new WeakMap();
-  let {initializer} = desc;
+  let { initializer } = desc;
 
   return {
     get() {
       let resource = resources.get(this);
 
       if (!resource) {
-        let {definition, args} = initializer.call(this);
+        let { definition, args } = initializer.call(this);
 
         resource = invokeHelper(this, definition, () => {
           let reified = args();
 
           if (Array.isArray(reified)) {
-            return {positional: reified};
+            return { positional: reified };
           }
 
           return reified;
