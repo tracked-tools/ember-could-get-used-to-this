1c0dac45b3a49b4fb5c211ee2bea54e73dc7a56d
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-08 19:11:05 -0500

    Create reproduction / failing test

--- tests/integration/use-test.js
+++ tests/integration/use-test.js
@@ -1,5 +1,8 @@
 import { module, test } from 'qunit';
 import { tracked } from 'tracked-built-ins';
+import { action } from '@ember/object';
+import { settled } from '@ember/test-helpers';
+import { waitFor } from '@ember/test-waiters';
 
 import { use, Resource } from 'ember-could-get-used-to-this';
 
@@ -44,5 +47,77 @@ module('@use', () => {
     instance.text = 'world';
 
     assert.equal(instance.test, 'world');
+  });
+
+  module('async updates', function() {
+    test('it works', async function(assert) {
+      class Invoker extends Resource {
+        @tracked result = Infinity;
+
+        get value() {
+          return this.result;
+        }
+
+        @action
+        @waitFor
+        async invoke() {
+          let { fn, args } = this.args.named;
+
+          await Promise.resolve();
+
+          this.result = await fn(...args);
+        }
+
+        setup() {
+          this.invoke();
+        }
+
+        update() {
+          this.invoke();
+        }
+      }
+
+      class MyClass {
+        @tracked left = 0;
+        @tracked right = 0;
+
+        @use test = new Invoker(() => {
+          return {
+            named: {
+              fn: this.math,
+              args: [this.left, this.right],
+            }
+          }
+        });
+
+        @action
+        @waitFor
+        math(left, right) {
+          return Promise.resolve(left + right);
+        }
+      }
+
+      let instance = new MyClass();
+
+      assert.equal(instance.test, Infinity, 'initial value');
+      await settled();
+
+      assert.equal(instance.test, 0, 'after async functions run');
+
+      instance.left = 1;
+      await settled();
+
+      assert.equal(instance.test, 1);
+
+      instance.right = 3;
+      await settled();
+
+      assert.equal(instance.test, 4);
+
+      instance.left = 2;
+      await settled();
+
+      assert.equal(instance.test, 5);
+    });
   });
 });
