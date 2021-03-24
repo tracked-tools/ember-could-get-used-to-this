27edabb0b8434c255c88b5595ba86752b360af9c
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-24 17:32:24 -0400

    Apply lint:js --fix

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,50 +1,50 @@
-"use strict";
+'use strict';
 
-const getChannelURL = require("ember-source-channel-url");
+const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function () {
   return {
     useYarn: true,
     scenarios: [
       {
-        name: "ember-lts-3.24",
+        name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            "ember-source": await getChannelURL("release"),
+            'ember-source': await getChannelURL('release'),
           },
         },
       },
       {
-        name: "ember-release",
+        name: 'ember-release',
         npm: {
           devDependencies: {
-            "ember-source": await getChannelURL("release"),
+            'ember-source': await getChannelURL('release'),
           },
         },
       },
       {
-        name: "ember-beta",
+        name: 'ember-beta',
         npm: {
           devDependencies: {
-            "ember-source": await getChannelURL("beta"),
+            'ember-source': await getChannelURL('beta'),
           },
         },
       },
       {
-        name: "ember-canary",
+        name: 'ember-canary',
         npm: {
           devDependencies: {
-            "ember-source": await getChannelURL("canary"),
+            'ember-source': await getChannelURL('canary'),
           },
         },
       },
       {
-        name: "embroider",
+        name: 'embroider',
         npm: {
           devDependencies: {
-            "@embroider/core": "*",
-            "@embroider/webpack": "*",
-            "@embroider/compat": "*",
+            '@embroider/core': '*',
+            '@embroider/webpack': '*',
+            '@embroider/compat': '*',
           },
         },
       },
