8548ae5410de49b1f95c57c9b30600b5d5065708
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-24 17:32:11 -0400

    Add quotes ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,8 @@ module.exports = {
     browser: true
   },
   rules: {
-    'ember/no-jquery': 'error'
+    'ember/no-jquery': 'error',
+    'quotes': ['error', 'single'],
   },
   overrides: [
     // node files
